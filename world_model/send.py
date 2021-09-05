594702b58476d5ffada478026aa1b58df59b30e6
alex <format37@gmail.com>
2021-09-05 16:20:13 +0300

    new file:   requirements.txt 	new file:   send.py

---
+++ world_model/send.py
@@ -0,0 +1,20 @@
+import os
+import paramiko
+
+def send_file(host, username, password, local_file, remote_file):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username=username, password=password)
+    sftp = ssh.open_sftp()
+    sftp.put(local_file, remote_file)
+    sftp.close()
+    ssh.close()
+
+send_file(
+    host='192.168.1.23',
+    username='alex',
+    passord=os.environ('SFTP_PASSWORD',''),
+    local_file='session.npy',
+    remote_file='session.npy'
+    )
+print('sent')
