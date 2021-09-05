c7a82a73161d26a054d674ae8984f9384a77506d
alex <format37@gmail.com>
2021-09-05 13:51:33 +0300

    modified:   rs_head.py

--- world_model/rs_head.py
+++ world_model/rs_head.py
@@ -9,14 +9,14 @@ pipeline.start(config)
 
 start_time = time.time()
 i = 0
-frames = []
+images = []
 while True:
 	frames = pipeline.wait_for_frames()
 	depth = frames.get_depth_frame()
 	if not depth:
 		continue
-	frames.append(np.asanyarray(depth.get_data( )))
+	images.append(np.asanyarray(depth.get_data( )))
 	i += 1
 	if (time.time() - start_time > 3):
 		break
-print('images collected:', i, np.array(frames).shape)
+print('images collected:', i, np.array(images).shape)
