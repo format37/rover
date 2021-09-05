eaca55b12494e8c798b358e717b02b9de0302069
alex <format37@gmail.com>
2021-09-05 13:50:24 +0300

    modified:   rs_head.py

--- world_model/rs_head.py
+++ world_model/rs_head.py
@@ -4,20 +4,19 @@ import time
 
 pipeline = rs.pipeline()
 config = rs.config()
-#config.enable_stream(rs.stream.depth,width=640,height=480)
 config.enable_stream(rs.stream.depth,width=1280,height=720)
 pipeline.start(config)
 
 start_time = time.time()
 i = 0
-
+frames = []
 while True:
 	frames = pipeline.wait_for_frames()
 	depth = frames.get_depth_frame()
 	if not depth:
 		continue
-	depth_image = np.asanyarray(depth.get_data( )) # * depth_scale
+	frames.append(np.asanyarray(depth.get_data( )))
 	i += 1
 	if (time.time() - start_time > 3):
 		break
-print('images collected:', i)
+print('images collected:', i, np.array(frames).shape)
