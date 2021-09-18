8e3e125d9c1fd54ce5e64caeb6e8b26bdb7bc45d
alex <format37@gmail.com>
2021-09-18 22:40:20 +0300

    modified:   world_model/head.py

--- world_model/head.py
+++ world_model/head.py
@@ -6,7 +6,7 @@ kit = ServoKit(channels=16, address=0x42)
 
 print('start')
 delay = 0.1
-for i in range(95,180):
+for i in range(100,180):
 	kit.servo[0].angle = i
 	time.sleep(delay)
 
@@ -14,7 +14,7 @@ for i in range(0,180):
         kit.servo[0].angle = 180-i
         time.sleep(delay)
 
-for i in range(0,95):
+for i in range(0,100):
         kit.servo[0].angle = i
         time.sleep(delay)
 
