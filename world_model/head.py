5463507f70aed152cc0f68bc20c481d6ce0224ea
alex <format37@gmail.com>
2021-09-19 06:52:25 +0300

    modified:   world_model/head.py

--- world_model/head.py
+++ world_model/head.py
@@ -8,15 +8,15 @@ def main():
         print('start')
         delay = 0.1
         for i in range(100,180):
-                kit.servo[0].angle = i
+                kit.servo[1].angle = i
                 time.sleep(delay)
 
         for i in range(0,180):
-                kit.servo[0].angle = 180-i
+                kit.servo[1].angle = 180-i
                 time.sleep(delay)
 
         for i in range(0,100):
-                kit.servo[0].angle = i
+                kit.servo[1].angle = i
                 time.sleep(delay)
 
         print('stop')
