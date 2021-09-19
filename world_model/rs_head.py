16b95b5284289d4d2907be8074e12ac285bcd5b3
alex <format37@gmail.com>
2021-09-19 16:28:14 +0300

    modified:   world_model/rs_head.py

--- world_model/rs_head.py
+++ world_model/rs_head.py
@@ -5,34 +5,33 @@ from adafruit_servokit import ServoKit
 import asyncio
 
 
-def rotation_map(start_position, end_position, steps):	
+"""def rotation_map(start_position, end_position, steps):	
 	multiplier = (end_position - start_position) / steps
 	rot_map = dict()
 	for i in range(0, steps):
 		rot_map[i] = start_position + i * multiplier
-	return rot_map
+	return rot_map"""
 
 
 async def move_head():
 
-	delay = 0.1
+	delay = 0.1	
+	kit = ServoKit(channels=16, address=0x42)
+	print('servo ready')
 	servo_activity = True
-	kit = ServoKit(channels=16, address=0x42)	
+	while not cam_ready:
+		await asyncio.sleep(delay)
 	print('servo start')
-	
 	for servo_angle in range(100,180):
 			kit.servo[0].angle = servo_angle
-			#time.sleep(delay)
 			await asyncio.sleep(delay)
 
 	for servo_angle in range(0,180):
 			kit.servo[0].angle = 180-servo_angle
-			#time.sleep(delay)
 			await asyncio.sleep(delay)
 
 	for servo_angle in range(0,100):
 			kit.servo[0].angle = servo_angle
-			#time.sleep(delay)
 			await asyncio.sleep(delay)
 
 	servo_activity = False
@@ -40,13 +39,7 @@ async def move_head():
 
 
 async def camera_capture():
-	return 0
 	
-	"""head_rotation_map = rotation_map(
-		start_position=servo_start, 
-		end_position=servo_end, 
-		steps=int(lenght*100)
-		)"""
 	pipeline = rs.pipeline()
 	config = rs.config()
 	#config.enable_stream(rs.stream.depth,width=1280,height=720) # FPS = 30
@@ -58,9 +51,15 @@ async def camera_capture():
 	servo_states = []
 	start_time = time.time()
 	i = 0
+	delay = 0.1
 	cam_ready = True
-	
+	print('print camera ready')
+	while not servo_activity:
+		await asyncio.sleep(delay)
+	print('print camera start')
 	while True:
+		if not servo_activity:
+			break
 		frames = pipeline.wait_for_frames()
 		depth = frames.get_depth_frame()
 		if not depth:
@@ -74,10 +73,10 @@ async def camera_capture():
 
 	pipeline.stop()
 
-	print('saving', np.array(depth_images).shape, 'images')
+	print('camera saving', np.array(depth_images).shape, 'images')
 	np.save('servo.npy', np.array(servo_states))
 	np.save('depth.npy', depth_images)
-	print('saved')
+	print('camera saved')
 
 
 def main():
@@ -93,8 +92,6 @@ def main():
 	loop = asyncio.get_event_loop()
 	loop.run_until_complete(asyncio.gather(move_head(), camera_capture()))
 	loop.close()
-
-	#await asyncio.gather(move_head(), camera_capture())
     
 
 if __name__ == '__main__':
