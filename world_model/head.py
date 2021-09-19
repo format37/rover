554149784fb06417a4ad01902cea02eb2840ccfa
alex <format37@gmail.com>
2021-09-19 06:50:01 +0300

    modified:   world_model/head.py

--- world_model/head.py
+++ world_model/head.py
@@ -1,21 +1,27 @@
 import time
 from adafruit_servokit import ServoKit
 
-# servo init
-kit = ServoKit(channels=16, address=0x42)
+def main():
+        # servo init
+        kit = ServoKit(channels=16, address=0x42)
 
-print('start')
-delay = 0.1
-for i in range(100,180):
-	kit.servo[0].angle = i
-	time.sleep(delay)
+        print('start')
+        delay = 0.1
+        for i in range(100,180):
+                kit.servo[0].angle = i
+                time.sleep(delay)
 
-for i in range(0,180):
-        kit.servo[0].angle = 180-i
-        time.sleep(delay)
+        for i in range(0,180):
+                kit.servo[0].angle = 180-i
+                time.sleep(delay)
 
-for i in range(0,100):
-        kit.servo[0].angle = i
-        time.sleep(delay)
+        for i in range(0,100):
+                kit.servo[0].angle = i
+                time.sleep(delay)
 
-print('stop')
+        print('stop')
+        return 0
+
+
+if __name__ == "__main__":
+	main()
