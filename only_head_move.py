2f6f9b04ff64f638a8f4e475f783555f612a3693
alex <format37@gmail.com>
2022-11-05 00:11:21 +0400

    modified:   only_head_move.py

--- only_head_move.py
+++ only_head_move.py
@@ -4,20 +4,20 @@ from adafruit_servokit import ServoKit
 # servo init
 kit = ServoKit(channels=16, address=0x42)
 
-print('start')
-
-# head
 delay = 0.01
+print('90,180')
 for i in range(90,180):
-	kit.servo[0].angle = i
+	kit.servo[1].angle = i
 	time.sleep(delay)
 
+print('0,180')
 for i in range(0,180):
-        kit.servo[0].angle = 180-i
+        kit.servo[1].angle = 180-i
         time.sleep(delay)
 
+print('0,90')
 for i in range(0,90):
-        kit.servo[0].angle = i
+        kit.servo[1].angle = i
         time.sleep(delay)
 
 print('stop')
