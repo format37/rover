5a0463134eff5197fadb0b8a64761fcfb90bddf7
alex <format37@gmail.com>
2022-11-05 00:13:55 +0400

    modified:   only_head_move.py

--- only_head_move.py
+++ only_head_move.py
@@ -7,17 +7,17 @@ kit = ServoKit(channels=16, address=0x42)
 delay = 0.01
 print('90,180')
 for i in range(90,180):
-	kit.servo[1].angle = i
+	kit.servo[0].angle = i
 	time.sleep(delay)
 
 print('0,180')
 for i in range(0,180):
-        kit.servo[1].angle = 180-i
+        kit.servo[0].angle = 180-i
         time.sleep(delay)
 
 print('0,90')
 for i in range(0,90):
-        kit.servo[1].angle = i
+        kit.servo[0].angle = i
         time.sleep(delay)
 
 print('stop')
