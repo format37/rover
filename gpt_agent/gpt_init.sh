f8ad5bf6948b462519f608ee9e4d0091b2f7bc3f
alex <format37@gmail.com>
2022-11-06 13:09:48 +0400

    new file:   gpt_init.sh

---
+++ gpt_agent/gpt_init.sh
@@ -0,0 +1,10 @@
+# check is openai.token exists, then create it, with ask user to run token
+if [ ! -f /home/openai/.openai/token ]; then
+    # input token and save to file
+    echo "Please input your token:"
+    read token
+    echo $token > openai.token
+    echo "Token saved sucessfully!"
+else
+    echo "Token already exists!"
+fi
