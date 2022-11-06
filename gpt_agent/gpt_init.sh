7136684a76304c1342f16fb587e8d4af4605d366
alex <format37@gmail.com>
2022-11-06 13:23:58 +0400

    modified:   gpt_init.sh 	new file:   gpt_request.py

--- gpt_agent/gpt_init.sh
+++ gpt_agent/gpt_init.sh
@@ -1,6 +1,7 @@
 # check is openai.token exists, then create it, with ask user to run token
 if [ ! -f /home/openai/.openai/token ]; then
     # input token and save to file
+    echo "Tokens are stored in https://beta.openai.com/account/api-keys"
     echo "Please input your token:"
     read token
     echo $token > openai.token
---
+++ gpt_agent/gpt_request.py
@@ -0,0 +1,39 @@
+import logging
+from datetime import datetime as dt
+import json
+import openai
+
+# enable logging
+logging.basicConfig(level=logging.INFO)
+
+
+def text_davinci(prompt, stop_words):
+    # read token from file openai.token
+    with open('openai.token', 'r') as f:
+        token = f.read()
+        # remove newline character
+        token = token[:-1]
+        print(token)
+
+    openai.api_key = token
+    return json.loads(str(openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=prompt,
+      temperature=0.9,
+      max_tokens=150,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0.6,
+      stop=stop_words
+    )))
+
+
+logging.info(str(dt.now())+' call_voice.openai conversation')
+prompt = """# Bubble sorting sample:
+"""
+stop_words = ['#']
+davinchi_response = text_davinci(str(prompt), stop_words)
+answer = davinchi_response['choices'][0]['text']
+logging.info(str(dt.now())+' call_voice.openai conversation answer: '+str(answer))
+total_tokens = davinchi_response['usage']['total_tokens']
+logging.info(str(dt.now())+' call_voice.openai conversation total_tokens: '+str(total_tokens))
