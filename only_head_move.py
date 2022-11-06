8971d4c725bf11b5fc915755af8c34ee80599c22
alex <format37@gmail.com>
2022-11-06 18:03:50 +0400

    modified:   ../only_head_move.py

--- only_head_move.py
+++ only_head_move.py
@@ -27,8 +27,8 @@ def move_head(answer, last_head_position):
         return new_head_position
 
 last_head_position = 90
-last_head_position = move_head('Look left', last_head_position)
-last_head_position = move_head('Look right', last_head_position)
-last_head_position = move_head('Look front', last_head_position)
+last_head_position = move_head('I look to the left', last_head_position)
+last_head_position = move_head('I look to the right', last_head_position)
+last_head_position = move_head('I look ahead', last_head_position)
 
 print('stop')
