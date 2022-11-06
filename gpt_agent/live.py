a04646f45fa78c7f673074e1e63d41ee2cb1f0a5
alex <format37@gmail.com>
2022-11-06 20:54:46 +0400

    modified:   live.py

--- gpt_agent/live.py
+++ gpt_agent/live.py
@@ -50,8 +50,8 @@ def camera_capture_single_nondepth_image():
         stream_type = rs.stream.color,
         width = 1920,
         height = 1080,
-        format = rs.format.y16,
-        framerate = 30
+        format = rs.format.bgr8,
+        framerate = 15
         )
     # config.enable_stream(rs.stream, 0, 1920, 1080, rs.format.bgr8, 30)
     pipeline.start(config)
