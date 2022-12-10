b374870bffc3ed96758532a7b40bcc90ba495b76
alex <format37@gmail.com>
2022-12-10 17:52:32 +0400

    modified:   sound_test.py

--- gpt_agent/sound_test.py
+++ gpt_agent/sound_test.py
@@ -4,13 +4,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# beep linux
-# os.system('play -nq -t alsa synth {} sine {}'.format(0.1, 1000))
-
 # Recording
-# arecord -D plughw:2,0 -f S16_LE -r 48000 -c 2 test.wav
 logger.info('Recording 5 seconds...')
-os.system('arecord test.wav --device=sysdefault:CARD=Device -f S16_LE -r 48000 -c 2 --duration=5')
+os.system('arecord test.wav --device=plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 --duration=4')
 
 # Playing
 logger.info('Playing')
