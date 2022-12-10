0a8e4df78ec50136300fee633154da4748d65b7f
alex <format37@gmail.com>
2022-12-10 16:53:45 +0400

    modified:   beep.py

--- gpt_agent/beep.py
+++ gpt_agent/beep.py
@@ -1,3 +1,4 @@
 import os
 # beep linux
-os.system('play -nq -t alsa synth {} sine {}'.format(0.1, 1000))
+# os.system('play -nq -t alsa synth {} sine {}'.format(0.1, 1000))
+os.system('aplay ru.wav --device=plughw:CARD=Device,DEV=0')
