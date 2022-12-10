cbf29ebb570f4eab3db611108767e8923952c57f
alex <format37@gmail.com>
2022-12-10 17:24:59 +0400

    modified:   beep.py

--- gpt_agent/beep.py
+++ gpt_agent/beep.py
@@ -1,4 +1,10 @@
+# https://www.waveshare.com/wiki/Audio_Card_for_Jetson_Nano
 import os
 # beep linux
 # os.system('play -nq -t alsa synth {} sine {}'.format(0.1, 1000))
-os.system('aplay ru.wav --device=plughw:CARD=Device,DEV=0')
+
+# Recording
+# arecord -D plughw:2,0 -f S16_LE -r 48000 -c 2 test.wav
+os.system('arecord --device=plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 -c 2 test.wav')
+# Playing
+os.system('aplay test.wav --device=plughw:CARD=Device,DEV=0')
