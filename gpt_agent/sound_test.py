c081f6866c8c09cd5ec7d6117d95f4ba37dd5e6d
alex <format37@gmail.com>
2022-12-10 17:29:32 +0400

    renamed:    beep.py -> sound_test.py

--- gpt_agent/sound_test.py
+++ gpt_agent/sound_test.py
@@ -1,10 +1,17 @@
 # https://www.waveshare.com/wiki/Audio_Card_for_Jetson_Nano
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+
 # beep linux
 # os.system('play -nq -t alsa synth {} sine {}'.format(0.1, 1000))
 
 # Recording
 # arecord -D plughw:2,0 -f S16_LE -r 48000 -c 2 test.wav
-os.system('arecord --device=plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 -c 2 test.wav')
+logger.info('Recording...')
+os.system('arecord test.wav -device=plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 -c 2 --duration=5')
+
 # Playing
+logger.info('Playing')
 os.system('aplay test.wav --device=plughw:CARD=Device,DEV=0')
