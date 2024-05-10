3a761d1bf4694512b6afda648be9c1385b4db2e6
Alex <format37@gmail.com>
2024-05-10 18:26:18 +0400

    modified:   ../rover/serve_movement.py 	modified:   agent.py

--- langchain/rover/serve_movement.py
+++ langchain/rover/serve_movement.py
@@ -1,10 +1,63 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import logging
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+import time
+from adafruit_servokit import ServoKit
 
 app = Flask(__name__)
+
+# enable logging
+logging.basicConfig(level=logging.INFO)
+
+def set(pca, track,speed,direction):
+	if speed>0:
+		frequency = speed*2300
+		direction = direction*0xffff
+		pca[track].frequency = int(frequency)
+		pca[track].channels[1].duty_cycle = int(direction)
+		pca[track].channels[0].duty_cycle = 0x7fff  #go
+	else:
+		pca[track].channels[0].duty_cycle = 0       #stop
 
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify({'message': 'ok'})
 
+@app.route('/move_track', methods=['POST'])
+def move_track():
+    data = request.get_json()
+    left_speed = data['left_speed']
+    right_speed = data['right_speed']
+    default_speed = 0.1
+
+    # tracks init
+    logging.info('Init tracks')
+    i2c_bus = busio.I2C(SCL, SDA)
+    pca = [
+        PCA9685(i2c_bus,address=0x40),
+        PCA9685(i2c_bus,address=0x41)
+        ]
+    # # servo init
+    # logging.info('Init servo')
+    # kit = ServoKit(channels=16, address=0x42)
+    # tracks go front
+    logging.info('tracks go front')
+    set(pca, track = 0, speed = default_speed, direction = 0)
+    set(pca, track = 1, speed = default_speed, direction = 1)
+    time.sleep(1)
+    # tracks stop
+    logging.info('tracks stop')
+    set(pca, track = 0, speed = 0, direction = 1)
+    set(pca, track = 1, speed = 0, direction = 1)
+    logging.info('done')
+
+    return jsonify({'message': 'done'})
+
+
 if __name__ == '__main__':
+	
+    
+	
     app.run(host='0.0.0.0', port=5000)
--- langchain/server/agent.py
+++ langchain/server/agent.py
@@ -1,17 +1,46 @@
 import requests
 
-rover_address = '192.168.100.17'
+class Movement:
+    def __init__(self, rover_address, port):
+        self.rover_address = rover_address
+        self.port = port
+        self.base_url = f'http://{rover_address}:{port}'
 
-def test_api():
-    url = f'http://{rover_address}:5000/test'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        print('Server response:')
-        print(data)
-    else:
-        print(f'Request failed with status code: {response.status_code}')
+    def test_api(self):
+        url = f'{self.base_url}/test'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            if data['message'] == 'ok':
+                print('Movement API: OK')
+                return True
+        else:
+            print(f'Request failed with status code: {response.status_code}')
+            return False
+
+    def move_head(self, pan, tilt):
+        url = f'{self.base_url}/move_head'
+        data = {'pan': pan, 'tilt': tilt}
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+            print('Head movement command sent successfully.')
+        else:
+            print(f'Request failed with status code: {response.status_code}')
+
+    def move_track(self, left_speed, right_speed):
+        url = f'{self.base_url}/move_track'
+        data = {'left_speed': left_speed, 'right_speed': right_speed}
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+            print('Track movement command sent successfully.')
+        else:
+            print(f'Request failed with status code: {response.status_code}')
 
 if __name__ == '__main__':
-    test_api()
+    rover_address = '192.168.100.17'
+    movement_port = 5000
+    movement = Movement(rover_address, movement_port)
+    movement.test_api()
