48bdce04b2e79d00e9e1defca67875438e865987
Alex <format37@gmail.com>
2024-05-10 15:53:09 +0400

    new file:   sound_record_system.py

---
+++ tests/sound_record_system.py
@@ -0,0 +1,13 @@
+# https://www.waveshare.com/wiki/Audio_Card_for_Jetson_Nano
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Recording
+logger.info('Recording 5 seconds...')
+os.system('arecord output.wav --device=plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 --duration=4')
+
+# Playing
+logger.info('Playing')
+os.system('aplay output.wav --device=plughw:CARD=Device,DEV=0')
