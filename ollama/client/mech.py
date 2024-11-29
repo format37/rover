aad29040d871516846d9ae70d544439042dc0b14
Alex <format37@gmail.com>
2024-11-30 02:12:50 +0400

    modified:   ollama/client/mech.py

--- ollama/client/mech.py
+++ ollama/client/mech.py
@@ -3,15 +3,14 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 import time
+import math
 from adafruit_servokit import ServoKit
 
 class RobotController:
-    def __init__(self, default_speed=0.1, head_movement_delay=0.02):
+    def __init__(self, default_speed=0.1):
         self.default_speed = default_speed
-        self.head_movement_delay = head_movement_delay
         self.init_logging()
         self.init_tracks()
-        # Uncomment to enable servo control
         self.init_servo()
 
     def init_logging(self):
@@ -34,6 +33,7 @@ class RobotController:
     def init_servo(self):
         self.logger.info('Initializing servo')
         self.servo_kit = ServoKit(channels=16, address=0x42)
+        self.head_servo = self.servo_kit.servo[0]
 
     def set_track(self, track, speed, direction):
         if speed > 0:
@@ -45,14 +45,16 @@ class RobotController:
         else:
             self.pca[track].channels[0].duty_cycle = 0  # Stop
 
-    def move_head(self, start_angle, end_angle):
-        """Move the robot's head from start_angle to end_angle"""
-        self.logger.info(f'Moving head from {start_angle} to {end_angle} degrees')
-        step = 1 if end_angle > start_angle else -1
-        for angle in range(start_angle, end_angle, step):
-            # Uncomment to enable servo control
-            # self.servo_kit.servo[0].angle = angle
-            time.sleep(self.head_movement_delay)
+    def smooth_move_head(self, start_angle, end_angle, duration=2, steps=200):
+        """Move the robot's head smoothly from start_angle to end_angle"""
+        self.logger.info(f'Moving head smoothly from {start_angle} to {end_angle} degrees')
+        
+        for i in range(steps + 1):
+            t = i / steps
+            # Ease in-out using sine function
+            angle = start_angle + (end_angle - start_angle) * (math.sin(t * math.pi - math.pi/2) + 1) / 2
+            self.head_servo.angle = angle
+            time.sleep(duration / steps)
 
     def move_tracks(self, left_direction, right_direction, speed=None):
         """Move tracks with specified directions and speed"""
@@ -79,21 +81,27 @@ class RobotController:
 
     def execute_movement_sequence(self):
         # Turn left
+        self.logger.info('Starting left turn sequence')
         self.move_tracks(1, 1)  # Forward
-        self.move_head(90, 180)
+        self.smooth_move_head(90, 180)  # Look left
 
         # Turn right
+        self.logger.info('Starting right turn sequence')
         self.move_tracks(0, 0)  # Backward
-        self.move_head(180, 0)
+        self.smooth_move_head(180, 0)  # Look right
 
         # Move forward
+        self.logger.info('Starting forward sequence')
         self.move_tracks(0, 1)  # Turn
-        self.move_head(0, 90)
+        self.smooth_move_head(0, 90)  # Look forward
 
         # Move backward briefly
         self.logger.info('Moving backward briefly')
         self.move_tracks(0, 0)  # Backward
         time.sleep(1)  # Backward movement duration
+
+        # Return head to center
+        self.smooth_move_head(90, 90)
 
         # Stop
         self.stop_tracks()
