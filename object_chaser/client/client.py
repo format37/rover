253ab1ba798296935078175d166d804e07e3ec25
Alex <format37@gmail.com>
2025-05-10 16:12:47 +0400

    logging

--- object_chaser/client/client.py
+++ object_chaser/client/client.py
@@ -13,6 +13,13 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Add a console handler to output logs to the terminal
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 logger.info("Starting client")
 
 current_goal = 90  # Initial goal angle in degrees (0 to 180)
