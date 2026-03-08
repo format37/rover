a2a0465fdf368fd52deb83c9eea8caa095d26507
Alex <format37@gmail.com>
2026-03-08 23:27:27 +0400

    confidence_min 0

--- object_chaser/client/config.py
+++ object_chaser/client/config.py
@@ -9,7 +9,7 @@ CAMERA_FOV = 87 # RGB: 87° × 58°
 # Depth: 69° × 42°
 
 # Detection
-DETECTION_CONFIDENCE_MIN = 0.8 # Ignore detections below this confidence
+DETECTION_CONFIDENCE_MIN = 0.0 # Ignore detections below this confidence
 
 # Tracking thresholds
 FORWARD_HEAD_THRESHOLD = 5.0   # Max head deviation (deg) to allow forward driving
